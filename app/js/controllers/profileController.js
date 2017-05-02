de6cd3ff84576a6a0214802aba9e3b18a8f808da
Mana <sabrina.mardjoeki@gmail.com>
2017-05-02 19:51:36 +0200

    grumpy css

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -4,4 +4,7 @@ angular.module('app')
 UserService.getOne($stateParams.id).then(function(res){
 $scope.user = res.data;
 });
+$scope.openLink = function(index) {
+    window.open($scope.user.pictures[index].url, 'Mon image', 'menubar=no, scrollbars=no, top=200, left=200, width=600, height=400');
+};
     });
