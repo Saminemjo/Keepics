93527f05e2591065ab5abc43be51792007909cbc
Mana <sabrina.mardjoeki@gmail.com>
2017-05-17 18:26:03 +0200

    connect db to heroku mlab

--- server/api/db.js
+++ server/api/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/keepics');
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k');
     callback();
 };
