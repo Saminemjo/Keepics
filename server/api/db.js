c89626decc6f427a49406a46895969e8d3fc6dd0
Mana <sabrina.mardjoeki@gmail.com>
2017-05-17 20:00:24 +0200

    try to connect

--- server/api/db.js
+++ server/api/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k');
+    MONGODB_URI= 'mongodb://localhost:27017/keepics';
+    MONGOLAB_URI='mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k';
+    mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k');
     callback();
 };
