d1b1c272a31c5ae3c7b3ffa2d27f5b01fb4bc4cf
Mana <sabrina.mardjoeki@gmail.com>
2017-05-18 10:50:14 +0200

    try 2

--- server/api/db.js
+++ server/api/db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    MONGODB_URI= 'mongodb://localhost:27017/keepics';
-    MONGOLAB_URI='mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k';
-    mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k');
+    MONGODB_URI= 'mongodb://heroku_39pjh70k:lqsv0fvp5jai3t5q2mod01rf4f@ds143231.mlab.com:43231/heroku_39pjh70k';
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/keepics');
     callback();
 };
