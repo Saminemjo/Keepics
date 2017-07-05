18523115354ca86e99bee27f50bd14579435209c
Mana <sabrina.mardjoeki@gmail.com>
2017-07-05 14:34:14 +0200

    register with name on profile page

--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .controller('RegisterController', function($scope, $location, Auth) {
         $scope.register = function() {
-            Auth.register($scope.user).then(function(res) {
+            Auth.register($scope.user).then(function(result) {
                 $location.path('user/profile/' + result.data.user.name);
             });
         };
